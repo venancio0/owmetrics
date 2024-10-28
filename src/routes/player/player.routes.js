ee9880776073da747de2787126d6ec6f24e312c6
gabriel <gabvcampaner@gmail.com>
2024-10-28 15:56:29 -0300

    adjusting player route

--- src/routes/player/player.routes.js
+++ src/routes/player/player.routes.js
@@ -1,9 +1,5 @@
 const overwatch = require('overwatch-api');
 
-// const platform = 'pc';
-// const region = 'us';
-// const tag = 'GamersCCCP-1569';
-
 module.exports = function (app) {
   app.get("/player/:platform/:region/:tag", function (req, res) {
     overwatch.getProfile(req.params.platform, req.params.region, req.params.tag, (err, json) => {
